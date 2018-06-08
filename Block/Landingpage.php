58a229910e1a2cef751ab0603b3f2c378723c6f9
Giacomo Moscardini <gmoscardini@webgriffe.com>
2018-06-08 11:41:10 +0200

    Primo rilascio del modulo WaPoNe_HelloWorld

---
+++ Block/Landingpage.php
@@ -0,0 +1,30 @@
+<?php
+namespace WaPoNe\HelloWorld\Block;
+
+class Landingpage extends \Magento\Framework\View\Element\Template
+{
+    private $_productCollectionFactory;
+
+    public function __construct
+    (
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+        array $data
+    )
+    {
+        parent::__construct($context, $data);
+
+        $this->_productCollectionFactory = $productCollectionFactory;
+    }
+
+    public function getProducts()
+    {
+        $collection = $this->_productCollectionFactory->create();
+
+        $collection
+            ->addAttributeToSelect('*')
+            ->setOrder(5);
+
+        return $collection;
+    }
+}
